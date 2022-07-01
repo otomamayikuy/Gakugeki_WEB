d4277cfb6cb4d59b0a6c4ac70f456e04aec1601f
RI0501 <brightray48@zeus.eonet.ne.jp>
2022-07-01 14:11:07 +0900

    ライブラリ変更

--- src/App.js
+++ src/App.js
@@ -3,17 +3,11 @@ import VideoPlayer from './Components/VideoPlayer';
 import './App.css';
 
 function App() {
-  const style = {
-    width: "50%",
-    margin: "0 auto",
-    marginTop: 150,
-  };
   return (
     <div className="App">
-      <div style={style}>
+        Gakugeki
         <VideoPlayer 
         VideoURL = 'https://mazwai.com/videvo_files/video/free/2018-01/small_watermarked/171124_H1_006_preview.webm'/>
-      </div>
     </div>
   );
 }
--- src/Components/VideoPlayer.js
+++ src/Components/VideoPlayer.js
@@ -1,22 +1,30 @@
-import React from 'react'
-import { ReactMediaRecorder } from 'react-media-recorder'
+import React, {Component} from 'react'
+import "video-react/dist/video-react.css"
+import { Player, ControlBar, PlaybackRateMenuButton } from 'video-react'
 
-const VideoPlayer = (props) => {
-
-  return (
-    <div>
-      <ReactMediaRecorder
-        video
-        render={({ status, startRecording, stopRecording }) => (
-          <div>
-            <p>{status}</p>            
-            <video src={props.VideoURL} controls/>
-          </div>
-        )}
-      />
-      
-    </div>
-  );
+class VideoPlayer extends React.Component {
+  componentDidMount() {
+    this.player.playbackRate = 1;
+    this.forceUpdate();
+  }
+  render() {
+    return (
+      <Player
+        ref={c => {
+          this.player = c;
+        }}
+        playsInline
+      >
+        <ControlBar>
+          <PlaybackRateMenuButton order={7} rates={[2, 1.5, 1.0, 0.5]} />
+        </ControlBar>
+        <source
+          src={this.props.VideoURL}
+          type="video/mp4"
+        />
+      </Player>
+    );
+  }
 }
 
 export default VideoPlayer;
