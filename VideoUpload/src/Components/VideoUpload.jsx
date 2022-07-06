1b0040e80b75aeb15cb0956df3e927e4d8fc51d4
RI0501 <brightray48@zeus.eonet.ne.jp>
2022-07-07 06:44:35 +0900

    フォルダ構成変更

---
+++ VideoUpload/src/Components/VideoUpload.jsx
@@ -0,0 +1,18 @@
+import { getStorage, ref } from "firebase/storage";
+
+//root 
+
+const VideoUpload = (props) => {
+    const style = {
+        width:"100%",
+        textAlign: "left",
+        display:"inline-block",
+        backgroundColor: "#86b5eb"
+    }
+    return (
+        <header>
+        <div style = {style}>
+            <h1 class="Header">
+                <a> ヘッダー</a>
+            </h1>
+        </
